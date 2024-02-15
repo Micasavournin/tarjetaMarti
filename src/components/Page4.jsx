856edc96373989cb2b8e1625f7dfcdc9ce50cbd9
Mica Savournin <micaasavoo@gmail.com>
2024-02-15 16:47:04 -0300

    seg

--- src/components/Page4.jsx
+++ src/components/Page4.jsx
@@ -6,7 +6,7 @@ const Page4 = () => {
             <img  src="https://firebasestorage.googleapis.com/v0/b/tarjeta-invitacion-2f4f0.appspot.com/o/asist-04.png?alt=media&token=0b8008cf-a46c-4ec6-aaae-2da20083a5b7" alt="" style={{ maxWidth: '100%', maxHeight: '100%' }} />
 
             <a 
-             style={{ position: 'absolute', top: '90%', left: '50%', transform: 'translate(-50%, -50%)'}}
+             style={{ position: 'absolute', top: '95%', left: '50%', transform: 'translate(-50%, -50%)'}}
         href={googleFormUrl} 
         target="_blank" rel="noopener noreferrer" 
         className=""
